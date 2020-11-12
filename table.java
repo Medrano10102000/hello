0f006ea1d726a3f505c53bcd4f4588bf06f502d3
Medrano10102000 <74008026+Medrano10102000@users.noreply.github.com>
2020-11-12 15:59:18 +0100

    Add files via upload

---
+++ table.java
@@ -0,0 +1,18 @@
+package abc;
+
+public class table {
+	
+	public static void main(String[] args) {
+		for (int i=0; i<6; i++) {
+			System.out.println(i);
+		}
+		System.out.println(" ___\n" + 
+				"|\n" + 
+				"|___\n" + 
+				"    |\n" + 
+				" ___|");
+		
+		
+	}
+
+}
